79d866d50e221c581dbde7549590ef07cfbe4276
ezreals-ares <nawaf303@gmail.com>
2024-09-05 13:33:51 +0700

    Menambahkan Keterangan Nama yang ke 2

--- MyFirstJavaGithub21.java
+++ MyFirstJavaGithub21.java
@@ -2,5 +2,6 @@ public class MyFirstJavaGithub21 {
 
     public static void main(String[] args) {
         System.out.println("Hello from Github");
+        System.out.println("Nama Saya Nawaf");
     }
 }
