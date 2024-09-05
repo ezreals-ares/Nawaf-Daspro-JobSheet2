83cc2995dc3a2c582ebdad210ae81129dfbb5a8b
ezreals-ares <nawaf303@gmail.com>
2024-09-05 13:24:54 +0700

    Revert "Menambahkan Keterangan Nama"
    This reverts commit dd53453901b59c4c6ee8743f658e503eae8d261f.

--- MyFirstJavaGithub21.java
+++ MyFirstJavaGithub21.java
@@ -2,7 +2,5 @@ public class MyFirstJavaGithub21 {
 
     public static void main(String[] args) {
         System.out.println("Hello from Github");
-        System.out.println("Nama Saya Nawaf");
-        
     }
 }
